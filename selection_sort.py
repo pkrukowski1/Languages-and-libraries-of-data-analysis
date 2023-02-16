82561842e774b6261aacd09d600d4812e473771c
pkrukowski1 <92364884+pkrukowski1@users.noreply.github.com>
2023-02-16 13:05:04 +0100

    Delete selection_sort.py

--- selection_sort.py
+++
@@ -1,9 +0,0 @@
-def main(lista):
-    for i in range(len(lista)-1):
-        min_elem = min(lista[i:])
-        idx_min = lista[i:].index(min_elem)
-        lista[i], lista[idx_min+i] = lista[idx_min+i], lista[i]
-    print(lista)
-
-if __name__ == "__main__":
-    main([2,1,1,3,3,5,10,12,5,5])
