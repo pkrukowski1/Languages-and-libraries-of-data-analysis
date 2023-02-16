049a0205195c88e9ffea447e4e00735678a78da4
pkrukowski1 <92364884+pkrukowski1@users.noreply.github.com>
2023-02-16 13:10:18 +0100

    Delete insertion_sort.py

--- insertion_sort.py
+++
@@ -1,9 +0,0 @@
-def main(lista):
-    for i in range(1,len(lista)):
-        for j in range(0,i):
-            if (lista[i] < lista[j]):
-                lista[i], lista[j] = lista[j], lista[i]
-    print(lista)
-
-if __name__ == '__main__':
-    main([1,2,4,1,20,2,4,14,16,3])
